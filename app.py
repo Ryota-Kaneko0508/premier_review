feb8db56fbdfaa3df2e3d342e8ad86ed4746a645
Ryota <kane2508ryo@gmail.com>
2023-03-08 15:32:58 +0900

    投稿機能を追加

--- app.py
+++ app.py
@@ -1,21 +1,44 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, g
 import sqlite3
 import os
 
-DATABASE="flaskmemo.db"
+DATABASE="premier.db"
 
 # create the app
 app = Flask(__name__)
     
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    if request.method == 'GET':
-        # reviews = Review.query.all()
-        return render_template('index.html')
+    reviews = get_db().execute("SELECT match, review FROM review").fetchall()
+    return render_template('index.html', reviews=reviews)
 
-@app.route("/create", methods={"GET", "POST"})
-def create():
-    if request.metod == 'POST':
-        match = request.form. get('title')
+@app.route("/post", methods={"GET", "POST"})
+def post():
+    if request.method == 'POST':
+        match = request.form.get('match')
+
+        if not match:
+            return redirect("/post")
+        
+        review = request.form.get('review')
+
+        if not review:
+            return redirect("/post")
+        
+        get_db().execute("INSERT INTO review (match, review) VALUES(?,?)",[match,review])
+        get_db().commit()
+
+        return redirect("/")
+    
+    return render_template('post.html')
+# database
+def connect_db():
+    rv = sqlite3.connect(DATABASE)
+    rv.row_factory = sqlite3.Row
+    return rv
+def get_db():
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
             
         
