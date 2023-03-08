a9e5fe3339598c51a81e68bf5542cc62a1ed7086
Ryota <kane2508ryo@gmail.com>
2023-03-08 16:41:51 +0900

    新規登録とログイン機能をつけた

--- app.py
+++ app.py
@@ -1,13 +1,91 @@
-from flask import Flask, render_template, request, redirect, g
+from flask import Flask, render_template, request, redirect, g, session, flash
 import sqlite3
+from flask_login import UserMixin, LoginManager, login_required, login_user, logout_user
+from werkzeug.security import generate_password_hash, check_password_hash
 import os
 
 DATABASE="premier.db"
 
 # create the app
 app = Flask(__name__)
-    
+app.secret_key = os.urandom(24)
+login_manager= LoginManager()
+login_manager.init_app(app)
+
+class User(UserMixin):
+    def __init__(self, userid):
+        self.id = userid
+
+## ログイン
+@login_manager.user_loader
+def load_user(userid):
+    return User(userid)
+@login_manager.unauthorized_handler
+def unauthorized():
+    return redirect('/login')
+
+@app.route("/logout", methods=['GET'])
+def logout():
+    session.clear()
+    logout_user()
+    return redirect('/login')
+
+@app.route("/signup", methods=['GET', 'POST'])
+def signup():
+    error_message = ''
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        pass_hash = generate_password_hash(password, method = 'sha256')
+
+        if not password and not username and pass_hash != password:
+            flash("入力に誤りがあります")
+            return redirect("/signup")
+        
+        db = get_db()
+        usercheck = get_db().execute("SELECT username from user WHERE username=?",[username,]).fetchall()
+        if not usercheck:
+            db.execute(
+                "INSERT INTO user (username,password) values(?,?)",
+                [username,pass_hash]
+            )
+            db.commit()
+            return redirect('/login')
+        else:
+            flash('入力されたユーザー名はすでに登録されています')
+        
+    return render_template('signup.html')
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    error_message = ''
+    username = ''
+
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        # ログインのチェック
+        user_data = get_db().execute(
+            "SELECT password FROM user WHERE username=?",[username,]
+        ).fetchone()
+
+        if username is not None:
+            if check_password_hash(user_data[0],password):
+                user_id = get_db().execute(
+                    "SELECT id FROM user WHERE username=?",[username,]
+                ).fetchone()
+                
+                session["user_id"] = user_id[0]
+                user = User(username)
+                login_user(user)
+                return redirect("/")
+            
+        flash('入力されたIDもしくはパスワードが誤っています')
+
+    return render_template('login.html')
+
 @app.route("/", methods=['GET', 'POST'])
+@login_required
 def index():
     reviews = get_db().execute("SELECT match, review FROM review").fetchall()
     return render_template('index.html', reviews=reviews)
